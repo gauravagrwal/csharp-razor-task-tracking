4e1a565e321a61d724abd878baa688e6ced792fd
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-03-25 11:28:46 +0530

    Completed.

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -18,6 +18,9 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+                return Page();
+
             _context.Tasks.Add(NewTask);
             _context.SaveChanges();
             return RedirectToPage("Index");
