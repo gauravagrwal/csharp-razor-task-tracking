dd6d75eb40441f87ea29fa5f8bd701e60d6bcd13
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-03-25 11:17:15 +0530

    Step 5 Completed

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,16 +1,20 @@
-using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+        public IEnumerable<Task> Tasks { get; set; }
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
