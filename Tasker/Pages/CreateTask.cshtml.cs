7ab4a89808656069dfc0e2a2532fdf476b618d02
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-03-25 11:30:10 +0530

    Update CreateTask.cshtml.cs

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -19,7 +19,9 @@ namespace Tasker.Pages
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
+            {
                 return Page();
+            }
 
             _context.Tasks.Add(NewTask);
             _context.SaveChanges();
