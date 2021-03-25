d5cc18fbedee1910edc1cc97045056a423b8f55d
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-03-25 10:33:15 +0530

    Step 3 Completed

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +5,15 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
